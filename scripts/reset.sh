c0a73f407505a407aac76478ec8268c1afa9df92
Andreas Dzialocha <adzialocha@users.noreply.github.com>
2023-06-04 20:21:00 +0200

    Delete reset.sh

--- scripts/reset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Stop all currently running processes
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/common.sh
-
-echo "Stop all running processes"
-reset_all
