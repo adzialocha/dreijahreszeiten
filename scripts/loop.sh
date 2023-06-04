6c189b6fffdb8f8aea4134ef38002c36ea69a12b
adz <x12@adz.garden>
2023-06-04 18:09:13 +0200

    Argument to play specific chapter, add figlet titles

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -17,10 +17,17 @@ BRIGHT_SIGN_PORT=3000
 # Arduino Nanos
 ARDUINO_1_HOST=192.168.1.100
 ARDUINO_1_PORT=3000
+
 ARDUINO_2_HOST=192.168.1.100
 ARDUINO_2_PORT=3000
+
 ARDUINO_3_HOST=192.168.1.100
 ARDUINO_3_PORT=3000
+
+print_title () {
+	figlet -f slant $1
+	echo ""
+}
 
 standby () {
 	seconds=$((($1 * 60) + $2))
@@ -90,62 +97,113 @@ play_epaper_video () {
 # Make sure to reset all processes before, just to be safe
 safe_reset_all
 
-# This is our main installation loop, running forever!
-while true
-do
-	# 0. Prolog
-	# 00:01:38.00
+# 0. Prolog
+# 00:01:38.00
+0_prolog () {
+	print_title "prolog"
 	# play_epaper_video 0_Prolog.mp4
 	# play_wide_video 0_Prolog.mp4
 	play_audio 0_Prolog.wav
 	standby 1 39
 	safe_reset_all
+}
 
-	# 1. Wasser
-	# 00:12:28.39
+# 1. Wasser
+# 00:12:28.39
+1_wasser () {
+	print_title "wasser"
 	play_epaper_video 1_Wasser.mp4
 	play_wide_video 1_Wasser.mp4
 	play_audio 1_Wasser.wav
 	standby 12 29
 	safe_reset_all
+}
 
-	# 2. Sonne
-	# 00:05:53.13
+# 2. Sonne
+# 00:05:53.13
+2_sonne () {
+	print_title "sonne"
 	# play_epaper_video 2_Sonne.mp4
 	# play_wide_video 2_Sonne.mp4
 	play_audio 2_Sonne.wav
 	standby 5 54
 	safe_reset_all
+}
 
-	# 3. Wind
-	# 00:07:19.58
+# 3. Wind
+# 00:07:19.58
+3_wind () {
+	print_title "wind"
 	# play_epaper_video 3_Wind.mp4
 	# play_wide_video 3_Wind.mp4
 	play_audio 3_Wind.wav
 	standby 7 20
 	safe_reset_all
+}
 
-	# 4. Anbahnung der Revolution
-	# 00:05:20.24
+# 4. Anbahnung der Revolution
+# 00:05:20.24
+4_anbahnung_der_revolution () {
+	print_title "anbahnung"
 	# play_epaper_video 4_AnbahnungDerRevolution.mp4
 	# play_wide_video 4_AnbahnungDerRevolution.mp4
 	play_audio 4_AnbahnungDerRevolution.wav
 	standby 5 21
 	safe_reset_all
+}
 
-	# 5. Feuer
-	# 00:03:16.86
+# 5. Feuer
+# 00:03:16.86
+5_feuer () {
+	print_title "feuer"
 	# play_epaper_video 5_Feuer.mp4
 	# play_wide_video 5_Feuer.mp4
 	play_audio 5_Feuer.wav
 	standby 3 18
 	safe_reset_all
+}
 
-	# 6. Revolution
-	# 00:20:00.00
+# 6. Revolution
+# 00:20:00.00
+6_revolution () {
+	print_title "revolution"
 	# play_epaper_video 6_Revolution.mp4
 	# play_wide_video 6_Revolution.mp4
 	play_audio 6_Revolution.wav
 	standby 20 1
 	safe_reset_all
-done
+}
+
+# This is our main installation loop, running forever!
+loop () {
+	while true
+	do
+		0_prolog
+		1_wasser
+		2_sonne
+		3_wind
+		4_anbahnung_der_revolution
+		5_feuer
+		6_revolution
+	done
+}
+
+if [[ -z $1 ]]; then
+	loop
+elif [[ $1 -eq 0 ]]; then
+	0_prolog
+elif [[ $1 -eq 1 ]]; then
+	1_wasser
+elif [[ $1 -eq 2 ]]; then
+	2_sonne
+elif [[ $1 -eq 3 ]]; then
+	3_wind
+elif [[ $1 -eq 4 ]]; then
+	4_anbahnung_der_revolution
+elif [[ $1 -eq 5 ]]; then
+	5_feuer
+elif [[ $1 -eq 6 ]]; then
+	6_revolution
+else
+	echo "! unknown chapter selected"
+fi
