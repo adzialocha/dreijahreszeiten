750a3ecaae4249ac069c721da011603486e40e8b
adz <x12@adz.garden>
2023-06-05 23:26:59 +0200

    Wait for arduino network

--- scripts/wait.sh
+++ scripts/wait.sh
@@ -4,17 +4,19 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/common.sh
 
-HTTP_PORT=80
-
+# Wait for the network to be ready
 if [[ -z "$SKIP_WAIT" ]]; then
-	echo "🢒 Wait for brightsign controller to be ready"
-	while ! nc -z $BRIGHT_SIGN_HOST $HTTP_PORT; do
+	echo "🢒 Wait for network"
+	while ! nc -z $ARDUINO_1_HOST $ARDUINO_1_PORT; do
 		sleep 0.5
 	done
 fi
 
+# Wait a little bit more just to be sure
+sleep 10
+
 # Clear terminal just because it is nice
 clear
 
-# Finally run the installation loop
+# Finally .. run the installation loop!
 $SCRIPT_DIR/loop.sh
