c1b1146482692525d9412848c0f4660b7a9f1b7b
adz <x12@adz.garden>
2023-06-09 16:12:36 +0200

    Correct way to check if network is online

--- scripts/wait.sh
+++ scripts/wait.sh
@@ -5,8 +5,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/common.sh
 
 wait_until_online () {
-	while ! nc -z $1 $2; do
-		sleep 0.5
+	while true; do
+		is_online=$(ping -c 1 $1 &> /dev/null)
+		if [[ $is_online -eq 0 ]]; then
+			break
+		else
+			sleep 0.5
+		fi
 	done
 }
 
@@ -16,8 +21,8 @@ if [[ -z "$SKIP_WAIT" ]]; then
 	wait_until_online $ARDUINO_1_HOST $ARDUINO_1_PORT
 	wait_until_online $ARDUINO_2_HOST $ARDUINO_2_PORT
 	wait_until_online $ARDUINO_3_HOST $ARDUINO_3_PORT
-	wait_until_online $BRIGHT_SIGN_HOST $BRIGHT_SIGN_PORT
-	echo "🢒 Wait for the right time to start installation (from $START_HOUR)"
+ 	wait_until_online $BRIGHT_SIGN_HOST $BRIGHT_SIGN_PORT
+	echo "🢒 Wait for the right time to start installation (from $START_HOUR:00)"
 	while [[ $(date +"%H") -lt $START_HOUR ]]; do
 		sleep 0.5
 	done
