b2ce0c2a44be675c76f8f0e0804c124f259368c6
adz <x12@adz.garden>
2023-06-09 19:29:53 +0200

    Back to old eink values

--- scripts/common.sh
+++ scripts/common.sh
@@ -51,7 +51,7 @@ ARDUINO_3_PORT=8888
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Clear the screen from ghosting artifacts every nth frame
-EPAPER_GHOST=72
+EPAPER_GHOST=32
 
 # Only take nth frame from 30fps video for epaper display to get to 5fps
 EPAPER_TAKE=6
